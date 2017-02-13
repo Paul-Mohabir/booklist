31f128aa18f9599d475fdb47125b43e4c2e818d5
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-12 19:37:39 -0500

    finished forgot to add git durring the process

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,17 @@
 class BooksController < ApplicationController
 
 
+def index
+  @books = Book.all
+  respond_to do |format|
+    format.html
+    format.json
+    books = Book.generate_csv(@books)
+    render :plain => books
+  end
+  format.json do |books|
+      render :json => @books
+    end
+  end
+
 end
